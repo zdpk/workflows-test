0b25bdabf8ff9c8782635be4d1689dfdff95625c
xezv <xezv@users.noreply.github.com>
2024-12-11 20:21:18 +0900

    [Test] Wed Dec 11 20:21:18 KST 2024

--- test.sh
+++ test.sh
@@ -12,5 +12,11 @@ git commit -m "[Test] $current_date"
 
 # Create a pull request
 git push origin HEAD
-gh pr create --title "[Test] $current_date" --body "This PR appends a character to test.txt"
+
+# Create a pull request using curl
+curl -u "xezv:TOKEN" \
+    -X POST \
+    -H "Accept: application/vnd.github.v3+json" \
+    https://api.github.com/repos/zdpk/workflows-test/pulls \
+    -d '{"title":"[Test] '"$current_date"'","body":"This PR appends a character to test.txt","head":"HEAD","base":"main"}'
 
