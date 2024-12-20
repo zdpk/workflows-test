d2dbf0f325bf86d7b4b479b999b78cbfc9b34271
xezv <xezv@users.noreply.github.com>
2024-12-20 12:39:04 +0900

    test commit

--- pr-test/new-pr.sh
+++ pr-test/new-pr.sh
@@ -43,14 +43,14 @@ delete_branch() {
 rand_branch_name=$(date +%s | sha256sum | base64 | head -c 32)
 target_branch_name="main"
 
-start_time=$(date +%s%3N)
+start_time=$(date +%s%N)
 create_test_branch "$rand_branch_name"
 create_pr "$rand_branch_name" "$target_branch_name"
 
 while true; do
     pr_info=$(fetch_pr_info "$rand_branch_name" "$target_branch_name")
-    end_time=$(date +%s%3N)
-    elapsed_time=$((end_time - start_time))
+    end_time=$(date +%s%N)
+    elapsed_time=$(( (end_time - start_time) / 1000000 ))
     echo "After $elapsed_time ms from start"
     if [[ -n $pr_info ]]; then
         echo "PR is created: $pr_info"
