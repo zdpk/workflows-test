e3bc5d67ba2be22c2282dead907ff623723298d2
xezv <xezv@users.noreply.github.com>
2024-12-22 19:41:17 +0900

    a

---
+++ pr-test/exist-pr.sh
@@ -0,0 +1,24 @@
+rand_branch_name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+rand_branch_name2=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+
+if git rev-parse "origin/$rand_branch_name" > /dev/null 2>&1; then
+    echo "Error: Not a git repository"
+    git switch "$rand_branch_name"
+else
+    git switch -c "$rand_branch_name"
+fi
+
+if git rev-parse "origin/$rand_branch_name" > /dev/null 2>&1; then
+    echo "Already exists"
+    git switch "$rand_branch_name"
+else
+    git switch -c "$rand_branch_name"
+    echo "$rand_branch_name" > a
+    git add .
+    git commit -m "a"
+    git push origin "$rand_branch_name"
+fi
+
+res=$(gh pr list --base "$rand_branch_name" "$rand_branch_name2")
+
+echo "res=$res"
